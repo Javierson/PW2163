1bdedae0b5da68b257f1c2c0785a9529cdb7667f
Javierson <warrior_alfa92@hotmail.com>
2016-09-20 11:50:34 -0600

    Enlaces

---
+++ enlaces.php
@@ -0,0 +1,80 @@
+
+<html>
+<head>
+<title>
+	P&aacute;gina de enlaces
+</title>
+
+</head>
+<body>
+
+Enlace a otra p&aacute;agina: <br>
+<a href = "google.com" tarjet = "_blank">
+	Google
+	</a>
+	<br>
+	Enlace a p&aacute; de nuestro sitio: <br>
+	<a href = "ejemplo01.html"> Ejemplo 01 </a>
+	Enlace en nuestra misma pa&acute;gina: <br>
+
+	Men&cute;
+
+	<ul>
+		<li>
+			<a href="#Primero">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
+<li>
+			<a href="#Segundo">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
+		<li>
+			<a href="#Terceso">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
+		<li>
+			<a href="#Cuarto">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
+		<li>
+			<a href="#Quinto">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
+		<li>
+			<a href="#Sexto">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
+	</ul>
+
+<p>
+	<a name = "Primero"></a>
+	<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+<a name = "Segundo"></a>
+	<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+<a name = "Tercero"></a>
+	<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+<a name = "Cuarto"></a>
+	<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+<a name = "Quinto"></a>
+	<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+<a name = "Sexto"></a>
+	<p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+
+</p>
+
+
+</body>
+
+</html>
