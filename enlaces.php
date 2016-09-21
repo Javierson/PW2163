36c20002c104b7c4f3433d7742686af757026b97
Javierson <warrior_alfa92@hotmail.com>
2016-09-21 11:53:45 -0600

    Enlaces

--- enlaces.php
+++ enlaces.php
@@ -61,10 +61,31 @@ Enlace a otra p&aacute;agina: <br>
 <p>
 	<a name = "Primero"></a>
 	<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+
+		<li>
+			<a href = "#Men&ucute;">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
 <a name = "Segundo"></a>
 	<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+
+<li>
+			<a href = "#Men&ucute;">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
 <a name = "Tercero"></a>
 	<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+
+<li>
+			<a href = "#Men&ucute;">
+			Primer p&aacute;arrafo
+		</a>
+		</li>
+
 <a name = "Cuarto"></a>
 	<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
 <a name = "Quinto"></a>
