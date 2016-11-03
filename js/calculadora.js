eea835c621642ba6450edbefd35e2b938e768c54
Javierson <warrior_alfa92@hotmail.com>
2016-11-03 11:59:33 -0700

    Calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,55 @@
+
+//Variable global
+
+var operador = "";
+
+function operadores(ope) // +, -, *, /
+{
+	operador = ope;
+}
+
+function igual()
+{
+	var valor1 = document.calculadora.operando1.value;
+	var valor2 = document.calculadora.operando2.value;
+	var resultado = 0;
+
+	if(operador == "+")
+		resultado = valor1 + valor2;
+	
+	if(operador == "-")
+		resultado = valor1 - valor2;
+
+	if(operador == "*")
+		resultado = valor1 * valor2;
+	
+	if(operador == "/")
+		resultado = valor1 / valor2;
+
+	document.calculadora.resultado = eval(valor1 + operador + valor2);
+	
+}
+
+function numeros(num)
+{
+	if (operador == "") // Escribir el operando1
+	{
+	// Guardamos el valor del operando uno
+	var valor = document.calculadora.operando1.value;
+	if(valor=="0")
+	{
+		document.calculadora.operando1.value = "";
+	}
+	document.calculadora.operando1.value = document.calculadora.operando1.value + num;
+}
+else // Escribir el operando2
+{
+	// Guardamos el valor del operando dos
+var valor = document.calculadora.operando2.value;
+	if(valor=="0")
+	{
+		document.calculadora.operando1.value = "";
+	}
+	document.calculadora.operando2.value = document.calculadora.operando2.value + num;
+}
+}
