90e2ed4be0c501879f15e0ac3e4e837bc98ddb05
Javierson <warrior_alfa92@hotmail.com>
2016-11-17 11:52:26 -0700

    Uso del JQuary

--- js/eventos.js
+++ js/eventos.js
@@ -11,10 +11,13 @@ var inicio = function() //main
   	url: 'https://randomuser.me/api/',
   	dataType: 'json',
   	success: function(data) {
+  	$("#imgFoto").show("slow");
+  	$("#txtNombre").show("slow");
   	// console.log(data.results[0].gender);
   	$("#txtNombre").val(data.results[0].name.first + " " + data.results[0].name.last);
   	$("#imgFoto").attr("src", data.results[0].picture.large);
   	console.log(data.results[0].name.first + " " + data.results[0].name.last);
+  	$("#miArticle").html("Texto").show();
   	}
 	});
 
